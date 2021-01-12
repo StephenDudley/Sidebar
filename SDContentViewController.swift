33b7a5b40bc5afa87cba27a4c63608ace8fd4f7b
StephenDudley <31688954+StephenDudley@users.noreply.github.com>
2021-01-12 06:10:52 -0500

    Add files via upload

---
+++ SDContentViewController.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+class SDContentViewController: NSViewController
+{
+    @IBOutlet var myLabel: NSTextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
